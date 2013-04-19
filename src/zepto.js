867fa2156246b1d6b9dec905318f01b09168053a
Daiz <taneli.vatanen@gmail.com>
2013-04-20 01:05:26 +0300

    Use self

--- src/zepto.js
+++ src/zepto.js
@@ -4,7 +4,7 @@
 
 var Zepto = (function() {
   var undefined, key, $, classList, emptyArray = [], slice = emptyArray.slice, filter = emptyArray.filter,
-    document = window.document,
+    document = self.document,
     elementDisplay = {}, classCache = {},
     getComputedStyle = document.defaultView.getComputedStyle,
     cssNumber = { 'column-count': 1, 'columns': 1, 'font-weight': 1, 'line-height': 1,'opacity': 1, 'z-index': 1, 'zoom': 1 },
